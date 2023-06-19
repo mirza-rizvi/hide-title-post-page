880fa95a2c401d01fec6e40373df27b09147d2ce
Mike Kipruto <e@kipmyk.co.ke>
2023-06-20 00:09:51 +0300

    Refactored code to OOP
    Refactored code to utilize the object-oriented approach.

--- hide-title-post-page.php
+++ hide-title-post-page.php
@@ -6,62 +6,66 @@
  * Author: Mirza Rizvi Amin
  */
 
-// Add a checkbox to the edit post screen for hiding the title tag
-function hide_title_meta_box() {
-    add_meta_box(
-        'hide_title_meta_box',
-        'Hide Title',
-        'hide_title_meta_box_callback',
-        'post',
-        'side',
-        'default'
-    );
-    add_meta_box(
-        'hide_title_meta_box',
-        'Hide Title',
-        'hide_title_meta_box_callback',
-        'page',
-        'side',
-        'default'
-    );
-}
-add_action('add_meta_boxes', 'hide_title_meta_box');
+class Hide_Title_Plugin {
 
-// Display the checkbox in the meta box
-function hide_title_meta_box_callback($post) {
-    $hide_title = get_post_meta($post->ID, 'hide_title', true);
-    ?>
-    <label for="hide_title">
-        <input type="checkbox" name="hide_title" id="hide_title" value="1" <?php checked(1, $hide_title, true); ?>>
-        Hide the title tag on this post or page
-    </label>
-    <?php
-}
+    public function __construct() {
+        add_action('add_meta_boxes', array($this, 'add_meta_boxes'));
+        add_action('save_post', array($this, 'save_hide_title_meta_box'));
+        add_filter('the_title', array($this, 'hide_title'), 10, 2);
+    }
 
-// Save the checkbox value when the post or page is saved
-function save_hide_title_meta_box($post_id) {
-    if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) {
-        return;
+    public function add_meta_boxes() {
+        add_meta_box(
+            'hide_title_meta_box',
+            'Hide Title',
+            array($this, 'hide_title_meta_box_callback'),
+            'post',
+            'side',
+            'default'
+        );
+        add_meta_box(
+            'hide_title_meta_box',
+            'Hide Title',
+            array($this, 'hide_title_meta_box_callback'),
+            'page',
+            'side',
+            'default'
+        );
     }
-    if (!current_user_can('edit_post', $post_id)) {
-        return;
-    }
-    if (isset($_POST['hide_title'])) {
-        update_post_meta($post_id, 'hide_title', 1);
-    } else {
-        delete_post_meta($post_id, 'hide_title');
-    }
-}
-add_action('save_post', 'save_hide_title_meta_box');
 
-// Remove the title tag if it is hidden
-function hide_title($title, $id = null) {
-    if (is_single() || is_page()) {
-        $hide_title = get_post_meta(get_the_ID(), 'hide_title', true);
-        if ($hide_title) {
-            return '';
+    public function hide_title_meta_box_callback($post) {
+        $hide_title = get_post_meta($post->ID, 'hide_title', true);
+        ?>
+        <label for="hide_title">
+            <input type="checkbox" name="hide_title" id="hide_title" value="1" <?php checked(1, $hide_title, true); ?>>
+            Hide the title tag on this post or page
+        </label>
+        <?php
+    }
+
+    public function save_hide_title_meta_box($post_id) {
+        if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) {
+            return;
+        }
+        if (!current_user_can('edit_post', $post_id)) {
+            return;
+        }
+        if (isset($_POST['hide_title'])) {
+            update_post_meta($post_id, 'hide_title', 1);
+        } else {
+            delete_post_meta($post_id, 'hide_title');
         }
     }
-    return $title;
+
+    public function hide_title($title, $id = null) {
+        if (is_single() || is_page()) {
+            $hide_title = get_post_meta(get_the_ID(), 'hide_title', true);
+            if ($hide_title) {
+                return '';
+            }
+        }
+        return $title;
+    }
 }
-add_filter('the_title', 'hide_title', 10, 2);
+
+$hide_title_plugin = new Hide_Title_Plugin();
